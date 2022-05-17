60224911c5ebcb25f7d40d897fa69e55803c7ec3
Sathish Kumar <chesathish5201@gmail.com>
2022-05-17 16:58:38 +0530

    Class for socket

--- Proxy.py
+++ Proxy.py
@@ -1,37 +1,43 @@
-import socket as s     #Socket
-import re              #Regex
+import socket as s  # Socket
+import re  # Regex
 import datetime
 
 class VPN:
-    def __init__(self,ip,span):
+    def __init__(self, ip, span):
         self.ip = ip
         self.span = span
-        self.filter()   #call the method when the object initialise
-        
+        self.filter()  # call the method when the object initialise
 
     def filter(self):
-        x = re.search("((127.0.0.1))", self.ip) 
+        x = re.search("((127.0.0.1))", self.ip)
         if x:
-            f = open("VPN_log.log","a")
-            f.write("\n"+self.ip+"\t"+str(span))
+            f = open("VPN_log.log", "a")
+            f.write("\n"+self.ip+"\t"+str(self.span))
             f.close()
         else:
             return 0
 
-ip = "127.0.0.1" # Alterable
-port = 9999      # Alterable
+class Proxy(VPN):
+    def __init__(self, ip, port):
+        self.ip = ip          # Alterable
+        self.port = port      # Alterable
+        self.socket()
 
-socket = s.socket(s.AF_INET,s.SOCK_STREAM)
-socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-socket.bind((ip,port))
-socket.listen()
+    def socket(self):
+        socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+        socket.bind((self.ip,self.port))
+        socket.listen()
 
-while True:
-    conn,ip  = socket.accept()
-    conn.send("Proxy server".encode())
-    span = datetime.datetime.now()  #IP login time
-    vpn = VPN(ip[0],span)
-    conn.close()
-    socket.close()
-    break
-    
+        while True:
+            conn, ip = socket.accept()
+            conn.send("Proxy server".encode())
+            span = datetime.datetime.now()  # IP login time
+            vpn = VPN(ip[0], span)
+            conn.close()
+            socket.close()
+            break
+
+ip = "127.0.0.1"
+port = 9999
+p1 = Proxy(ip,port)
