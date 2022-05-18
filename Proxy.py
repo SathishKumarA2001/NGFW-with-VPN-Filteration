6e9235b5ffe8ca394f8b24954aeb67f20f87573d
Sathish Kumar <chesathish5201@gmail.com>
2022-05-18 22:50:16 +0530

    proxy to server communication

--- Proxy.py
+++ Proxy.py
@@ -1,6 +1,20 @@
 import socket as s  # Socket
 import re  # Regex
 import datetime
+from threading import Thread
+import requests,mimetypes
+
+resp_on = """HTTP/1.1 200 OK
+Connection: Keep-Alive
+Content-Type: text
+Accept-Ranges: bytes
+Content-Length: {length}
+Vary: Accept-Encoding
+
+{body}
+
+"""
+
 
 class VPN:
     def __init__(self, ip, span):
@@ -33,27 +47,44 @@ class VPN:
                     f.close()
                     break
 
-class Proxy(VPN):
-    def __init__(self, ip, port):
-        self.ip = ip          # Alterable
-        self.port = port      # Alterable
-        self.socket()
+class Balancer(Thread):
+    def __init__(self,conn,ip):
+        Thread.__init__(self)
+        self.conn = conn
+        self.ip = ip
 
-    def socket(self):
-        socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-        socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-        socket.bind((self.ip,self.port))
-        socket.listen()
+    def run(self):
+        data = conn.recv(4098)
+        #req = data.decode()
+        resp = requests.get("http://127.0.0.1/")
+        with open('buffer.html','w') as buffer:
+            buf = buffer.write(resp.text) 
+        with open('buffer.html','r') as buffer:
+            buf = buffer.read()
+        conn.sendall(resp_on.format(type=mimetypes.guess_type(buf)[0],length=len(buf),body=buf).encode())
+        
 
-        while True:
-            conn, ip = socket.accept()
-            conn.send("Proxy server".encode())
-            span = datetime.datetime.now()  # IP login time
-            vpn = VPN(ip[0], span)
-            conn.close()
-            socket.close()
-            break
 
 ip = "127.0.0.1"
 port = 9999
-p1 = Proxy(ip,port)
+
+socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+socket.bind((ip,port))
+socket.listen()
+
+    
+while True:
+    conn, ip = socket.accept()
+    #conn.send("Proxy server".encode())
+    span = datetime.datetime.now()  # IP login time
+    VPN(ip[0], span)  #Object constructed
+    Balancer(conn,ip).start()
+            
+            
+"""            while True:
+                data = conn.recv(1024)
+                print(data.decode())
+            #conn.close()
+            #socket.close()
+            break  """
